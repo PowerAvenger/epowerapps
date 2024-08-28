b650a4a4760982904c9a71a5827cd0629aa45b6a
Jose <>
2024-08-28 08:29:42 +0200

    añadida app demanda

--- powerapps.py
+++ powerapps.py
@@ -13,34 +13,45 @@ st.subheader('Powered by Jose Vidal',divider='rainbow')
 url_linkedin = "https://www.linkedin.com/posts/jfvidalsierra_powerapps-activity-7216715360010461184-YhHj?utm_source=share&utm_medium=member_desktop"
 st.write("Recomienda, comenta y comparte en [Linkedin](%s)" % url_linkedin)
 
-col1,col2,col3,col4=st.columns(4)
-with col1:
+col11,col12,col13,col14=st.columns(4)
+with col11:
     st.markdown("Cálculo de indexados")
     st.image('images/telemindex.jpg')
     
-with col2:
+with col12:
     st.markdown(':rainbow[OMIE a todo color]')
     st.image('images/escalacv.jpg')
     
-with col3:
+with col13:
     st.markdown('Compara FIJO vs PVPC')
     st.image('images/fijovspvpc.jpg')
 
-with col4:
+with col14:
     st.markdown('Simula indexados')
     st.image('images/simulindex.jpg')
     
 with st.container():
-    col1,col2,col3,col4=st.columns(4)
-    with col1:
+    col21,col22,col23,col24=st.columns(4)
+    with col21:
         url1 = "https://telemindex-josevidal.streamlit.app/"
         st.write("Calcula los precios medios de indexado con [Telemindex](%s)" % url1)
-    with col2:
+    with col22:
         url2 = "https://escalacvpy-josevidal.streamlit.app/"
         st.write("Disfruta OMIE a todo color basado en la [Escala Cavero-Vidal](%s)" % url2)
-    with col3:
+    with col23:
         url3 = "https://fijovspvpcpy-josevidal.streamlit.app/"
         st.write("Calcula el impacto del margen en el Tp [Fijo vs PVPC](%s)" % url3)
-    with col4:
+    with col24:
         url4 = "https://simulindexpy-josevidal.streamlit.app/"
         st.write("Simula los precios de indexado a un año vista [Simulindex](%s)" % url4)
+
+col31,col32,col33,col34=st.columns(4)
+with col31:
+    st.markdown("Demanda Real")
+    st.image('images/demanda.jpg')
+
+with st.container():
+    col41,col42,col43,col44=st.columns(4)
+    with col41:
+        url5 = "https://demanda-josevidal.streamlit.app/"
+        st.write("Simula la demanda real [Demanda](%s)" % url5)
