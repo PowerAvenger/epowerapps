614ad2f7558721491f5b21383bec39f536372ca3
Jose <>
2024-11-17 21:25:05 +0100

    añadido redata epowerapp

--- powerapps.py
+++ powerapps.py
@@ -3,17 +3,17 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="PowerAPPs",
+    page_title="ePowerAPPs",
     page_icon=":bulb:",
     layout='wide'
 )
-st.title(':orange[e]PowerAPPs disponibles by Jose Vidal')
+st.title(':orange[e]PowerAPPs© disponibles by Jose Vidal')
 #st.subheader('Powered by Jose Vidal',divider='rainbow')
 
 url_linkedin = "https://www.linkedin.com/posts/jfvidalsierra_powerapps-activity-7216715360010461184-YhHj?utm_source=share&utm_medium=member_desktop"
 st.write("Recomienda, comenta y comparte en [Linkedin](%s)" % url_linkedin)
 
-col11,col12,col13,col14=st.columns(4)
+col11,col12,col13,col14,col15=st.columns(5)
 with col11:
     #st.markdown("Cálculo de indexados")
     url1 = "https://telemindex-josevidal.streamlit.app/"
@@ -41,30 +41,36 @@ with col14:
     st.subheader('Simulindex',divider='rainbow')
     st.write("Simula el precio medio de indexado a un año vista con [Simulindex](%s)" % url4)
     st.image('images/simulindex.jpg')
-    
-
-col31,col32,col33,col34=st.columns(4)
-with col31:
+with col15:
     #st.markdown("Demanda Real")
     url5 = "https://demanda-josevidal.streamlit.app/"
     st.subheader('Demanda Real',divider='rainbow')
     st.write("Simula la [demanda real](%s). Se incluye producción por autoconsumo" % url5)
     st.image('images/demanda.jpg')
-with col32:
+    
+
+col21,col22,col23,col24,col25=st.columns(5)
+with col21:
     #st.markdown("Marginales")
     url6 = "https://marginales-by-josevidal.streamlit.app/"
     st.subheader('Tecnologías Marginales',divider='rainbow')
     st.write("Impacto de las tecnologías [marginales](%s) en el precio del mercado" % url6)
     st.image('images/marginales.jpg')
-with col33:
+with col22:
     #st.markdown("Excedentes")
     url7 = "https://excedentes-josevidal.streamlit.app/"
     st.subheader('Autoconsumo: Excedentes',divider='rainbow')
     st.write("Compara el coste regulado de tus [excedentes](%s) con la oferta en fijo" % url7)
     st.image('images/excedentes.jpg')
-with col34:
+with col23:
     url8 = "https://omipvsomie-josevidal-superporraomie2024.streamlit.app/"
     st.subheader('Especial superporreros2024',divider='rainbow')
     st.write("¿Bates a OMIP en la lucha por el [MVPStarPower2024](%s)?" % url8)
     st.image('images/omipvsomie.jpg')
+with col24:
+    url9 = "https://redata-by-josevidal.streamlit.app/"
+    st.subheader('Infografías REData',divider='rainbow')
+    st.write("Otra forma de ver los datos de [REData](%s)" % url9)
+    st.image('images/redata.jpg')
+
 
