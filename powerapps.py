e8a5d5e4c3f625d01d1124b3f4c294550edf61ad
Jose <>
2025-04-01 22:36:28 +0200

    actualizada plantilla

--- powerapps.py
+++ powerapps.py
@@ -13,14 +13,15 @@ url_bluesky = 'https://bsky.app/profile/poweravenger.bsky.social'
 
 url_linkedin = "https://www.linkedin.com/posts/jfvidalsierra_powerapps-activity-7216715360010461184-YhHj?utm_source=share&utm_medium=member_desktop"
 url_bluesky = 'https://bsky.app/profile/poweravenger.bsky.social'
-st.write("Recomienda, comenta y comparte en [Bluesky](%s). Gracias a :orange[Jesús López] por el empujoncito con el python." % url_bluesky)
+#st.write("Recomienda, comenta y comparte en [Bluesky](%s). Gracias a :orange[Jesús López] por el empujoncito con el python." % url_bluesky)
+st.markdown(f"Deja tus comentarios y propuestas en mi perfil de [Linkedin]({url_linkedin}) - ¡Sígueme en [Bluesky]({url_bluesky})!")
 
 col11,col12,col13,col14,col15=st.columns(5)
 with col11:
     #st.markdown("Cálculo de indexados")
-    url1 = "https://telemindex-josevidal.streamlit.app/"
-    st.subheader('Telemindex',divider='rainbow')
-    st.write("Calcula los precios medios de indexado con [Telemindex](%s)" % url1)
+    url1 = "https://minorista-josevidal.streamlit.app/"
+    st.subheader('Mercado minorista de la electriciad',divider='rainbow')
+    st.write("Toda la información del mercado [minorista](%s)  de la electricidad" % url1)
     st.image('images/telemindex.jpg')
     
 with col12:
@@ -37,17 +38,12 @@ with col13:
     st.write("¿Ganas o pierdes?: Compara tu [Fijo vs PVPC](%s)" % url3)
     st.image('images/fijovspvpc.jpg')
 
-with col14:
-    #st.markdown('Simula indexados')
-    url4 = "https://simulindex-by-josevidal.streamlit.app/"
-    st.subheader('Simulindex',divider='rainbow')
-    st.write("Simula el precio medio de indexado a un año vista con [Simulindex](%s)" % url4)
-    st.image('images/simulindex.jpg')
+
 with col15:
-    #st.markdown("Demanda Real")
+    #st.markdown("Demanda + Autoconsumo")
     url5 = "https://demanda-josevidal.streamlit.app/"
-    st.subheader('Demanda Real',divider='rainbow')
-    st.write("Simula la [demanda real](%s). Se incluye producción por autoconsumo" % url5)
+    st.subheader('Demanda + Autoconsumo',divider='rainbow')
+    st.write("Simula el [CONSUMO](%s) como la demanda más el autoconsumo" % url5)
     st.image('images/demanda.jpg')
     
 
@@ -74,7 +70,7 @@ with col23:
     st.subheader('Infografías REData',divider='rainbow')
     st.write("Otra forma de ver los datos de [REData](%s)" % url9)
     st.image('images/redata.jpg')
-with col24:
+with col14:
     url10 = "https://spo2425-josevidal.streamlit.app/"
     st.subheader('SPO: Super Power OMIE 2024-2025',divider='rainbow')
     st.write("Gana el [MVPStarPower](%s) del año y bate a OMIP!" % url10)
